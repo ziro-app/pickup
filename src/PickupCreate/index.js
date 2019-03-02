af0ebe40aabfb30e0e82e5af1431d834af6c83dc
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-03-02 15:16:21 -0800

    add axios cancel token

--- src/PickupCreate/index.js
+++ src/PickupCreate/index.js
@@ -1,4 +1,7 @@
+/* import libraries */
 import React, { Component } from 'react'
+import { CancelToken } from 'axios'
+/* import methods */
 import { initialUiState, changeUiState } from './methods/stateMachine'
 import fetchInitialData from './methods/fetchInitialData'
 import updatePickup from './methods/updatePickup'
@@ -14,11 +17,13 @@ export default class PickupCreate extends Component {
 		pickup_code: ''
 	}
 	/*-- methods --*/
+	cancelTokenSource = CancelToken.source()
 	changeUiState = changeUiState(this)
 	fetchInitialData = fetchInitialData(this)
 	updatePickup = updatePickup(this)
 	renderForm = renderForm(this)
 	/*-- lifecycle --*/
 	componentDidMount = () => this.fetchInitialData()
+	componentWillUnmount = () => this.cancelTokenSource.cancel()
 	render = () => this.renderForm(this.state.uiState)
 }
