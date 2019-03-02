292e9fc0614510a3a246a718f653b8404810d9c2
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-03-01 18:31:03 -0800

    add error on fetch component

--- src/PickupCreate/methods/renderForm.js
+++ src/PickupCreate/methods/renderForm.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import Header from '../../Header/index'
 import Spinner from '../../Spinner/index'
-// import ErrorOnFetch from '../../ErrorOnFetch/index'
+import ErrorOnFetch from '../../ErrorOnFetch/index'
 
 const renderForm = that => uiState => {
 	const componentToRender = {
 		default:
 			<div>Hello!</div>,
 		fetching:
-			<Spinner size={'8rem'} />
-		// error_fetching:
-		// 	<ErrorOnFetch />
+			<Spinner size={'8rem'} />,
+		error_fetching:
+			<ErrorOnFetch />
 	}
 	const ui = uiState !== 'fetching' && uiState !== 'error_fetching' ? 'default' : uiState
 	return <Header title='Solicitar retirada'>{componentToRender[ui]}</Header>
