a97def0536e293df2aae9c75aad9017e048749d7
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-03-02 16:24:28 -0800

    validate codes before sending

--- src/PickupCreate/utils/validateForm.js
+++ src/PickupCreate/utils/validateForm.js
@@ -1,10 +1,13 @@
+import { optionIsValid } from './validateFields'
+
 const validateForm = ({
 	pickup_code,
+	codes,
 	resellers
 }) => {
 	if (resellers) {
 		/* check if each field contains valid values */
-		const pickup_code_is_valid = Boolean(pickup_code)
+		const pickup_code_is_valid = optionIsValid(codes, pickup_code)
 		const formIsValid = pickup_code_is_valid
 		return {
 			formIsValid,
