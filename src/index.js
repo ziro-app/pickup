4c4144ac1bf4df22450cd0207aea679f061ff64e
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-03-03 07:54:01 -0800

    add day-picker styles to root

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react'
 import { render } from 'react-dom'
 import App from './App/index'
 import './index.css'
+import 'react-day-picker/lib/style.css'
 
 render(<App />, document.getElementById('app'))
