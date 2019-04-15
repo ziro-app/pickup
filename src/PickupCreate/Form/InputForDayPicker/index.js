81819a365e0d078367ec85325021102f6a6468ec
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-04-15 09:15:14 -0700

    prevent mobile keyboard to popup when date-picker is selected

--- src/PickupCreate/Form/InputForDayPicker/index.js
+++ src/PickupCreate/Form/InputForDayPicker/index.js
@@ -4,6 +4,7 @@ import { input, inputFocus } from './styles'
 export default class InputForDayPicker extends Component {
 	render = () =>
 		<input 
+			readOnly={true}
 			onKeyPress={event => event.preventDefault()}
 			style={this.props.value ? inputFocus : input}
 			{...this.props}
